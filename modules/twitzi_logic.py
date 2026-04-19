56b0807cad6afa087878f0e888a073b1c53e7bef
Abdul Samad K A <85991146+nuruszama@users.noreply.github.com>
2026-04-19 08:49:35 +0400

    Update twitzi_logic.py

--- modules/twitzi_logic.py
+++ modules/twitzi_logic.py
@@ -4,6 +4,6 @@ def process_logic(msg, bot_name, admin_id, token):
     if text == "/start":
         return {
             "type": "text", 
-            "data": "🐦 *Twitzi is Online*\nYour social media and notification bridge is active."
+            "data": f"🐦 *{bot_name} is Online*\nYour social media and notification bridge is active."
         }
     return None
