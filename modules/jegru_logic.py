23778240f6a002082915776d837b505489cbab46
Abdul Samad K A <85991146+nuruszama@users.noreply.github.com>
2026-04-25 22:15:17 +0400

    Update jegru_logic.py

--- modules/jegru_logic.py
+++ modules/jegru_logic.py
@@ -72,7 +72,7 @@ def process_logic(msg, bot_name, admin_id, token):
             
             if "document" in msg:
                 file_id = msg["document"]["file_id"]
-                file_name = msg["document"].get("caption").split('\n', 1)[0]
+                file_name = msg["document"].get("caption").split('\n')[0]
                 return {"type": "document", "data": file_id, "caption": file_name, "delete_original": True}
 
             if "photo" in msg:
@@ -94,7 +94,7 @@ def process_logic(msg, bot_name, admin_id, token):
 
         if "document" in msg:
             file_id = msg["document"]["file_id"]
-            file_name = msg["document"].get("caption").split('\n', 1)[0]
+            file_name = msg["document"].get("caption").split('\n')[0]
             return {"type": "document", "data": file_id, "caption": file_name}
 
         if "photo" in msg:
