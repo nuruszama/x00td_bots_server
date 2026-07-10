b429404cce946fc18163799db1dd840364ac425a
Xiaomi Creek <ask.in.uae@gmail.com>
2026-07-10 09:18:06 +0400

    Delete modules/ciper_logic.py

--- modules/ciper_logic.py
+++
@@ -1,9 +0,0 @@
-def process_logic(msg, bot_name, admin_id, token):
-    text = msg.get("text", "").lower().strip()
-    
-    if text == "/start":
-        return {
-            "type": "text", 
-            "data": "🔐 *Ciper-Wire is Online*\nSecure communication protocols initialized."
-        }
-    return None
